adb7f7ac9e5c49b4b376cfb63e15f363e0e5ed33
uvsq22004090 <71641885+uvsq22004090@users.noreply.github.com>
2021-02-19 11:39:09 +0100

    Update incendie.py

--- incendie.py
+++ incendie.py
@@ -1,1 +1,9 @@
-
+#########################################
+# groupe DLBI 1
+# Kubilay MEYDAN
+# Julie CIESLA
+# Alix FRAGNER
+# Margaux DUBOIS GUENRO
+# Elise REBER
+# https://github.com/uvsq-info/l1-python
+#########################################
